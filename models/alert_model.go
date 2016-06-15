717c91aca785795e61c56894d3cb594b7532ad39
MachineShop <machineshop@MachineShops-MacBook-Pro.local>
2016-06-14 22:28:06 -0600

    added allert model

--- models/alert_model.go
+++ models/alert_model.go
@@ -2,12 +2,15 @@ package models
 
 type Alert interface {
 	Run() error
-	GetDescription() (string, error)
+	GetDescription() string
+	GetMessage() string
 }
 
 type EmailAlert struct {
 	BaseModel
 	Name 		string                `json:"name"`
+	Description 	string                `json:"description"`
+	Message 	string                `json:"message"`
 }
 
 func (ea *EmailAlert) Run() error {
@@ -15,7 +18,11 @@ func (ea *EmailAlert) Run() error {
 	return nil
 }
 
-func (ea *EmailAlert) GetDescription() (string, error) {
-	//TODO implement
-	return "", nil
+func (ea *EmailAlert) GetDescription() string {
+	return ea.Description
 }
+
+func (ea *EmailAlert) GetMessage() string {
+	return ea.Message
+}
+
